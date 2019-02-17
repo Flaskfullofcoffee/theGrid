e7a9bdcd753b3355e08866761513ad52bfdfaccc
Zac <zdobbs3@gmail.com>
2019-02-17 16:08:07 -0700

    set search list up

--- scripts/index.js
+++ scripts/index.js
@@ -12,30 +12,61 @@ function createNode(element) {
 fetch(url)
   .then((resp) => resp.json())
   .then(json => {
-    let gameList = json.games;
-    for (var i = 0; i < gameList.length; i++) {
-      let games = gameList[i].game;
-      let ul = document.querySelector('ul');
-      let game = document.createElement('li');
-      game.textContent = games;
-      // game.className = 'game';
-      ul.appendChild(game);
-      console.log(games);
-
-
+    let gameList = json.games.map(obj => obj.game);
+    addGames(gameList);
     let searchBox = document.getElementById('searchbar');
-    let searchBtn = document.querySelector('.searchBtn');
-    let response = document.querySelector('.result');
-    let reset;
-
-    let checkSearch = () => {
-      // console.log(searchBox.value);
-      if  (searchBox.value !==  games) {
-        response.textContent = 'Game not Found';
-      } else {
-        response.textContent = `${searchBox.value} found`;
-      }
-    }
-    searchBtn.addEventListener('click', checkSearch);
-    }
+    searchBox.addEventListener('keyup', filterGames(gameList))
+    // for (var i = 0; i < gameList.length; i++) {
+    //   addGame(gameList[i]);
+    // }
+    // let searchBtn = document.querySelector('.searchBtn');
+    // searchBtn.addEventListener('click', search(gameList));
   })
+// function for adding game
+  let addGames = (gameList)  => {
+  let ul = document.createElement('ul');
+    gameList.forEach(game => {
+      let li = document.createElement('li');
+      li.textContent = game;
+      ul.appendChild(li)
+    })
+    let resultContainer = document.getElementById('game-list');
+    resultContainer.innerHTML = '';
+    resultContainer.appendChild(ul);
+  };
+
+  let filterGames = (gameList) => (event) => {
+    let input = event.target.value.toLowerCase();
+    let filteredGamesList = gameList.filter(game => {
+      return game.toLowerCase().includes(input);
+    })
+    addGames(filteredGamesList);
+  }
+
+
+
+
+
+
+
+
+
+
+
+  //
+  // const search = (gameList) => (event) => {
+  //   let searchBox = document.getElementById('searchbar');
+  //   let response = document.querySelector('.result');
+  //   let reset;
+  //   if  (gameList.includes(searchBox.value)) {
+  //     response.textContent = `${searchBox.value} found`;
+  //     console.log(event);
+  //   } else {
+  //     response.textContent = 'Game not Found';
+  //   }
+  // }
+
+
+
+
+  // set as a form submit event isntead of a event listener
